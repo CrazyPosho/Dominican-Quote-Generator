9f3ef8abfea6e21d94dbde442cd2c8fe2d59b0fe
David Hernandez <davidjorgeagramonte@gmail.com>
2024-06-29 22:18:15 -0400

    Se conecto con bases de datos local

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ const button = document.getElementById("new-quote");
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
 
+button.addEventListener("click", getContent);
+function getContent() {
+  fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const random = getRandomNumber();
+      assingPhrase(data, random);
+    });
+}
+function getRandomNumber() {
+  return Math.floor(Math.random() * 24);
+}
+function assingPhrase(data, random) {
+  quote.textContent = data[random].frase;
+  author.textContent = data[random].autor;
+}
+function refresh() {
+  location.reload();
+}
